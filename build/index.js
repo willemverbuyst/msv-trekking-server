f7a93f65d4d06628a7ddab4c5799ccfc21d98ca4
Willem Verbuyst <willemverbuyst@gmail.com>
2020-10-11 11:22:48 +0200

    Connect to mongoDB

--- build/index.js
+++ build/index.js
@@ -3,10 +3,22 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+var dotenv_1 = __importDefault(require("dotenv"));
 var express_1 = __importDefault(require("express"));
+var mongoose_1 = __importDefault(require("mongoose"));
 var trekRoutes_1 = require("./routes/trekRoutes");
 var constants_1 = require("./config/constants");
+dotenv_1.default.config();
 var app = express_1.default();
+var DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+mongoose_1.default
+    .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+})
+    .then(function () { return console.log('DB connection successful'); });
 app.use('/treks', trekRoutes_1.router);
 app.listen(constants_1.PORT, function () {
     console.log("listening on port " + constants_1.PORT);
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,26 @@
+import dotenv from 'dotenv';
 import express from 'express';
+import mongoose from 'mongoose';
 import { router as trekRouter } from './routes/trekRoutes';
 import { PORT } from './config/constants';
 
+dotenv.config();
+
 const app = express();
+
+const DB = process.env.DATABASE!.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD!
+);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('DB connection successful'));
 
 app.use('/treks', trekRouter);
 
